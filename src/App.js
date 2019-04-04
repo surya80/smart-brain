e5e982559232448d7fb709e1fc3fad90edfeb83e
SuryaHS <surja.gain@gmail.com>
2019-04-04 14:46:30 +0530

    commit initial

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
   onButtonSubmit = () => {
     // console.log('click');
     this.setState({imageURL: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://powerful-mountain-16561.herokuapp.com/imageurl', {
         method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
       .then(response => response.json())  
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://powerful-mountain-16561.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignin = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://powerful-mountain-16561.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
